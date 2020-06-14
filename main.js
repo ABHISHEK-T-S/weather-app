96fd86745c7ac497e5cc6d251621bc82d613946d
Abhishek T S <tsabhishek1999@gmail.com>
2020-06-14 22:11:03 +0530

    api sucessfully added

---
+++ main.js
@@ -0,0 +1,34 @@
+function getWeather() {
+  let temperature = document.getElementById("temp");
+  let humidity = document.getElementById("humidity");
+  let wind = document.getElementById("wind");
+  let pressure = document.getElementById("pres");
+
+
+
+    let url =
+    "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&lang=en&appid=06ce5938ce2ea2a5951e0f6bc624cb7b";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "  ° c";
+        let humidi = data.main.humidity;
+        humidity.innerHTML=humidi+"  %";
+        let windval = data.wind.speed;
+        wind.innerHTML=windval+"  km/hr";
+        let presval = data.main.pressure;
+        pressure.innerHTML=presval+"  pa";
+
+        
+      });
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+
+
+getWeather();
